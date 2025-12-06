883a8436397fdaee2f65993d2d1758a0d36819bc
Pablo Ortega <pablo_ortega@es.ibm.com>
2025-12-06 11:17:26 +0100

    Day 6 part 2 completed

--- day6.py
+++ day6.py
@@ -1,5 +1,36 @@
+import math
+
 SUM = "+"
-MULTI = "*"
+MULTI = "*"     
+
+def operateSpecial(matrix):
+    grandTotal = 0
+    
+    col = len(matrix[0]) - 1
+    l_numbers = []
+    while col >= 0:
+        number = ""
+        for row in range(0, len(matrix)):
+            value = matrix[row][col]
+            if row == len(matrix) - 1: # The last
+                if not value == SUM and not value == MULTI:
+                    if not number.replace(" ", "") == "":
+                        l_numbers.append(number)
+                else:
+                    if len(l_numbers) > 0:
+                        l_numbers.append(number)
+                        l_numbers = list(map(int, l_numbers))
+                        if value == SUM:
+                            grandTotal += sum(l_numbers)
+                        else: 
+                            grandTotal += math.prod(l_numbers)     
+                        l_numbers = []
+                col = col - 1
+                break               
+            else:
+                number = number + value
+    
+    return grandTotal
 
 def operate(matrix, col):
     operand = matrix[-1][col]
@@ -35,6 +66,22 @@ def number6_1():
         grand_total += operate(math_grid, i)
         
     print("Day 6 part 1: The grand total is " + str(grand_total))
+    
+def number6_2():
+    dataFile = open("./data/day6.txt", "r")
+    math_grid = []
+    
+    for line in dataFile:
+        line = line.replace("\n", "")
+        s_line = list(line)
+        
+        math_grid.append(s_line)
+    
+    grand_total = 0
+    grand_total = operateSpecial(math_grid)
+        
+    print("Day 6 part 2: The grand total is " + str(grand_total))
         
 number6_1()
+number6_2()
             
