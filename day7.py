c15c8453359c5035498ea67ec501662395c9a268
Pablo Ortega <pablo_ortega@es.ibm.com>
2025-12-08 18:28:04 +0100

    Day 7 part 2 completed

--- day7.py
+++ day7.py
@@ -37,6 +37,55 @@ def number7_1():
         
     print("Day 7.1, the number of splits are " + str(i_splits))
     
+def number7_2():
+    dataFile = open("data/day7.txt", "r")
+    
+    manifold = []
+    index = 0
+    row = 0
+    col = 0
+    for line in dataFile:
+        line = line.replace("\n", "")
+        s_line = list(line)
+        if START in s_line: 
+            row = index
+            col = s_line.index(START)
+        index += 1
+        manifold.append(list(line))
+    
+    l_tach = {
+        (row, col): 1
+    }
+
+    for i in range(0, len(manifold) - 1):
+        l_aux = dict()
+        for (r, c) in l_tach.keys():
+            if r < len(manifold) - 1: 
+                current_val = l_tach[(r, c)]
+                if manifold[r + 1][c] == SPACE:
+                    if not (r + 1, c) in l_aux.keys():
+                        l_aux[(r + 1, c)] = current_val
+                    else:
+                        l_aux.update({(r + 1, c): l_aux[(r + 1, c)] + current_val})
+                else:
+                    if not (r + 1, c - 1) in l_aux.keys():
+                        l_aux[(r + 1, c - 1)] = current_val
+                    else: 
+                        l_aux.update({(r + 1, c - 1): l_aux[(r + 1, c - 1)] + current_val})
+                    if not (r + 1, c + 1) in l_aux.keys():
+                        l_aux[(r + 1, c + 1)] = current_val
+                    else:
+                        l_aux.update({(r + 1, c + 1): l_aux[(r + 1, c + 1)] + current_val})
+        l_tach = l_aux.copy()
+        
+    timelines = 0
+    for key in l_tach.keys():
+        timelines += l_tach[key]
+        
+    print("Day 7.2, the number of timelines are " + str(timelines))
+    
+    
 number7_1()
+number7_2()
         
     
